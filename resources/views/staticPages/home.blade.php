0313732be40f09e46ad7b1a9a824a7af407e66f3
vagrant <vagrant@localhost.localdomain>
2019-12-08 05:56:35 +0000

    sign-up

--- resources/views/staticPages/home.blade.php
+++ resources/views/staticPages/home.blade.php
@@ -6,6 +6,6 @@
 <div class="row center jumbotron home-main center">
   <h1>Sample App<span class="text-danger">.</span></h1>
   <p class="center">This is the home page for the Laravel Sample Application</p>
-  <a href="{{ action('StaticPages@getHome') }}" class="btn btn-lg btn-primary center">Sign up Now!</a>
+  <a href="{{ action('UsersController@getSignup') }}" class="btn btn-lg btn-primary center">Sign up Now!</a>
 </div>
 @endsection
